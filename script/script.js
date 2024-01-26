67357e4103dac37f4d95aaab8a0d07a9c3931021
Eduardo Nogueira <edunogueira06@hotmail.com>
2024-01-26 08:47:51 -0300

    cod

---
+++ script/script.js
@@ -0,0 +1,11 @@
+var n1 = document.getElementById("numero1");
+var n2 = document.getElementById("numero2");
+var input = document.getElementsByClassName("button")
+document.get
+
+function numero1(){
+    if(n1 != 1){
+        n1.innerHTML = input.value;
+    };
+};
+    
