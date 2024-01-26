2347445b18ff8e0098d1b80d86976f0f17902568
Eduardo Nogueira <edunogueira06@hotmail.com>
2024-01-26 08:54:21 -0300

    save1

--- script/script.js
+++ script/script.js
@@ -1,11 +1,5 @@
-var n1 = document.getElementById("numero1");
-var n2 = document.getElementById("numero2");
-var input = document.getElementsByClassName("button")
-document.get
+function insert(num){
 
-function numero1(){
-    if(n1 != 1){
-        n1.innerHTML = input.value;
-    };
-};
-    
+    var numero = document.getElementById("numero1").innerHTML;
+    document.getElementById("numero1").innerHTML = numero + num;
+}
