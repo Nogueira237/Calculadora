bf77c90e2acdeef54f665092c9d8f3a747d2b172
Eduardo Nogueira <edunogueira06@hotmail.com>
2024-01-26 09:05:39 -0300

    calculadora meia boca finalizada

--- script/script.js
+++ script/script.js
@@ -3,3 +3,14 @@ function insert(num){
     var numero = document.getElementById("numero1").innerHTML;
     document.getElementById("numero1").innerHTML = numero + num;
 }
+
+function limpar(){
+    document.getElementById("numero1").innerHTML = "";
+}
+
+function calcular(){
+    var resultado = document.getElementById("numero1").innerHTML;
+    if(resultado){
+        document.getElementById('numero1').innerHTML = eval(resultado);
+    }
+}
